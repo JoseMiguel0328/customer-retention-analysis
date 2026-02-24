c2d8e97f778f3657d09399fb8805aa615ed95875
Astrocatt <josemiguel0328@gmail.com>
2026-02-24 00:29:17 -0500

    schema.sql updated

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,12 +2,18 @@ CREATE DATABASE IF NOT EXISTS customer_retention;
 USE customer_retention;
 
 CREATE TABLE transactions (
-	invoice 		VARCHAR(20),
-    stock_code 		VARCHAR(20),
-    description		VARCHAR(255),
-    quantity		INT,
-	invoice_date 	DATETIME,
-    price			DECIMAL(10, 2),
-    customer_id 	INT,
-    country			VARCHAR(100)
+    invoice         VARCHAR(20),
+    stock_code      VARCHAR(20),
+    description     VARCHAR(255),
+    quantity        INT,
+    invoice_date    DATETIME,
+    price           DECIMAL(10, 2),
+    customer_id     INT,
+    country         VARCHAR(100),
+
+    PRIMARY KEY (invoice, stock_code),
+
+    INDEX idx_customer_id   (customer_id),
+    INDEX idx_invoice_date  (invoice_date),
+    INDEX idx_country       (country)
 );
